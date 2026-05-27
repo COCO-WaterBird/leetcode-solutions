89af79b78fd0c95edab5e68290b8519aa1506b76
COCO-WaterBird <zhangkk533@gmail.com>
2026-05-27 14:51:03 -0700

    Time: 0 ms (100%), Space: 26.5 MB (44.62%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,12 +5,6 @@ class Solution:
         """
         n = len(nums)
         k = k % n
-        def reverse(l,r):
-            while l < r:
-                nums[l],nums[r] = nums[r],nums[l]
-                l += 1
-                r -= 1
-
-        reverse(0,n-1)
-        reverse(0,k-1)
-        reverse(k,n-1)
+        nums_sec = nums[n-k:]
+       
+        nums[:] = nums_sec + nums[0:n-k]
