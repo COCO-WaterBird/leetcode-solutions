b5baac2bc2174a75508b253f3d759e26ae29dbf4
COCO-WaterBird <zhangkk533@gmail.com>
2026-05-30 17:52:26 -0700

    Time: 3 ms (34.42%), Space: 19.2 MB (66.86%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,10 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        lookup = {"(":")","[":"]","{":"}"}
-        for p in s:
-            if p in lookup.keys():
-                stack.append(p)
-            elif len(stack) == 0 or lookup[stack.pop()] != p:
-                return False
-        return len(stack) == 0
+        pairs = {
+            "(":")",
+            "[":"]",
+            "{":"}",
+        }
+
+        for char in s:
+            if char in pairs.keys():
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+
+                if char != pairs[stack.pop()]:
+                    return False
+        return not stack
