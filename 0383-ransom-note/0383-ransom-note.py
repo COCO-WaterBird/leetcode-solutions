972bc4c9a27cefd1d6356af0732b3132692d7a12
COCO-WaterBird <zhangkk533@gmail.com>
2026-06-03 10:21:57 -0700

    Time: 9 ms (92.63%), Space: 19.8 MB (8.4%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,10 +1,10 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        count = {}
-        for c in magazine:
-            count[c] = count.get(c,0) + 1
-        for c in ransomNote:
-            if c not in count or count[c] == 0:
+        count_m = Counter(magazine)
+        m = len(ransomNote)
+        for char in ransomNote:
+            if char in count_m and count_m[char] != 0:
+                count_m[char] -= 1
+            else:
                 return False
-            count[c] -= 1
         return True
