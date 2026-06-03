a6b31beb4752e7802e9276543143632a2928c551
COCO-WaterBird <zhangkk533@gmail.com>
2026-06-03 14:35:40 -0700

    Time: 103 ms (79.06%), Space: 61.3 MB (84.29%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+
+        res = []
+        def backtrack(start, current):
+            if len(current) == k:
+                res.append(current[:])  # 注意要copy！
+                return
+            for i in range(start, n + 1):
+                current.append(i)
+                backtrack(i + 1, current)
+                current.pop()
+        backtrack(1, [])
+        return res        
