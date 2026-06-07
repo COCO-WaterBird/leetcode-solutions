f568fd4f421443887d752bbff29a5a437bcd60ac
COCO-WaterBird <zhangkk533@gmail.com>
2026-06-07 11:25:15 -0700

    Time: 1 ms (93.43%), Space: 20.2 MB (16.35%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,14 +1,15 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
 
-        res = 0
-        cur_end = 0    # 当前这跳的边界
-        max_reach = 0  # 历史最远能到哪
-
-        for i in range(len(nums) - 1):  # 注意：不用走到最后一个
-            max_reach = max(max_reach, i + nums[i])
-            if i == cur_end:       # 走到当前跳的边界了
-                res += 1           # 必须再跳一次
-                cur_end = max_reach  # 下一跳的边界 = 目前最远
-
-        return res
+        n = len(nums)
+        jumps = 0
+        current_end = 0
+        farthest = 0
+        
+        for i in range(n-1):
+            farthest = max(farthest, i + nums[i])
+            if i == current_end:
+                jumps += 1
+                current_end = farthest
+        
+        return jumps        
