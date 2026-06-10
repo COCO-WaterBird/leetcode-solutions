7d4c3a66e9239c41f80772f34a5c7e44fc0aedbc
COCO-WaterBird <zhangkk533@gmail.com>
2026-06-10 11:47:05 -0700

    Time: 4 ms (8.23%), Space: 19.4 MB (33.21%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -3,6 +3,7 @@ class Solution:
         m = len(obstacleGrid)
         n = len(obstacleGrid[0])
         dp = [[0]*n for _ in range(m)]
+
         for i in range(m):
             if obstacleGrid[i][0] == 1:
                 break
@@ -11,10 +12,13 @@ class Solution:
             if obstacleGrid[0][j] == 1:
                 break
             dp[0][j] = 1
+
         for i in range(1,m):
             for j in range(1,n):
+                
                 if obstacleGrid[i][j] == 1:
                     dp[i][j] = 0
                 else:
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+                    dp[i][j] = dp[i][j-1] + dp[i-1][j]
         return dp[m-1][n-1]
