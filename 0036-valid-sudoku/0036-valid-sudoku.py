88002a1e742532de1c27f7bcfd12b79d7895f1bb
COCO-WaterBird <zhangkk533@gmail.com>
2026-06-15 10:31:05 -0700

    Time: 4 ms (48.01%), Space: 19.2 MB (74.04%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,20 +1,20 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-       
+
         row = [[0]*10 for _ in range(10)]
         col = [[0]*10 for _ in range(10)]
         matrix = [[[0]*10 for _ in range(3)] for _ in range(3)]
         
         for i in range(9):
             for j in range(9):
-                num = board[i][j]
-                if num == ".":
+                val = board[i][j]
+                if val == ".":
                     continue
-                else:
-                    num = int(num)
-                    row[i][num] += 1
-                    col[j][num] += 1
-                    matrix[i//3][j//3][num] += 1
-                    if row[i][num] > 1 or col[j][num] > 1 or matrix[i//3][j//3][num] > 1:
-                        return False
-        return True        
+                num = int(val)
+                row[i][num] += 1
+                col[j][num] += 1
+                matrix[i//3][j//3][num] += 1
+                if row[i][num] > 1 or col[j][num] > 1 or matrix[i//3][j//3][num] > 1:
+                    return False
+        
+        return True
